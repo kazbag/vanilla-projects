7df1d4733475ed328a16b0b7e22ff81dab563428
kazbag <45149138+kazbag@users.noreply.github.com>
2019-11-04 19:58:51 +0100

    removed material design - this project is way too simple to use any framework tbh, added data fetch and some styles

---
+++ weather/app.js
@@ -0,0 +1,42 @@
+const api_key = "d9b6b27164757a07549519653faf6737";
+const searchbox = document.querySelector("#searchbox");
+const searchButton = document.querySelector("#search");
+const cardContainer = document.querySelector(".card-container");
+let city = "";
+const getCityValue = e => {
+  city = e.target.value;
+};
+
+const getWeather = async e => {
+  e.preventDefault();
+  const api_call = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric&lang=pl`
+  );
+  const response = await api_call.json();
+  getCard(response);
+};
+
+const getCard = response => {
+  response.cod === 200
+    ? (cardContainer.innerHTML = `
+    <div class="card">
+
+        <div class="city">
+        <img class="city-image" src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png"/>
+            <h3 class="city-name">${response.name}</h3>
+        </div>
+
+        <div class="city-info">
+            <h6>${response.weather[0].description}</h6>
+            <h6 class="city-temperature">
+                ${response.main.temp}&deg;C
+            </h6>
+        </div>
+
+    </div>
+    `)
+    : (cardContainer.innerHTML = "Podaj prawidłowe miasto");
+};
+
+searchbox.addEventListener("input", getCityValue);
+searchButton.addEventListener("click", getWeather);
