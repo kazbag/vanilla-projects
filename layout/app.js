da7d1920f3e2b85e61409c9230a1811d3e79d1b2
kazbag <45149138+kazbag@users.noreply.github.com>
2019-11-06 20:11:04 +0100

    added burger menu

--- layout/app.js
+++ layout/app.js
@@ -2,3 +2,13 @@
 particlesJS.load("particles-js", "assets/particles.json", function() {
   console.log("callback - particles.js config loaded");
 });
+
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger");
+const toggleNav = () => {
+  menu.classList.toggle("visible");
+  const items = [...document.querySelectorAll(".menu-item")];
+  items.forEach(item => item.classList.toggle("disp"));
+};
+
+hamburger.addEventListener("click", toggleNav);
