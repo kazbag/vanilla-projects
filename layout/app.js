a7784485356a1965e6abf8a4761e847a11b74f82
kazbag <45149138+kazbag@users.noreply.github.com>
2019-11-10 21:14:20 +0100

    fixed some strange bugs

--- layout/app.js
+++ layout/app.js
@@ -7,8 +7,6 @@ const menu = document.querySelector(".menu");
 const hamburger = document.querySelector(".hamburger");
 const toggleNav = () => {
   menu.classList.toggle("visible");
-  const items = [...document.querySelectorAll(".menu-item")];
-  items.forEach(item => item.classList.toggle("disp"));
 };
 
 hamburger.addEventListener("click", toggleNav);
