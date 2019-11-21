244a305e2f64fbbeb4f20b4b90789d745ecb189f
kazbag <45149138+kazbag@users.noreply.github.com>
2019-11-21 17:54:31 +0100

    added intervals handling + sound if time's out

--- cucumberro/app.js
+++ cucumberro/app.js
@@ -6,13 +6,16 @@ const pauseButton = document.querySelector("#pause-button");
 const resetButton = document.querySelector("#reset-button");
 const form = document.querySelector("form");
 const timeLeftSpan = document.querySelector("#time-left");
+const intervalsLeftSpan = document.querySelector(".intervals-left");
 let workMinutes = workMinutesInput.value;
 let intervalsAmount = intervalsAmountInput.value;
 let pauseMinutes = pauseMinutesInput.value;
 let isStarted = false;
 let isPaused = true;
 let hasAppBeenStarted = false;
+const audio = new Audio("./assets/sounds/ding.wav");
 // refactor, I think it will be better to create one function which pass inputName as parameter
+
 const setWorkMinutes = e => {
   e.preventDefault();
   if (!isStarted) {
@@ -46,13 +49,16 @@ const updateTime = input => {
   let time = input.value * 60;
   const updateTimeEachSecond = setInterval(() => {
     goButton.disabled = true;
-
     if (time <= 0) {
-      intervalsAmount > 0
-        ? intervalsAmount--
-        : (intervalsAmount = intervalsAmountInput.value);
+      audio.play();
+      if (intervalsAmount > 0) {
+        intervalsAmount--;
+      } else {
+        intervalsAmount = intervalsAmountInput.value;
+      }
       isStarted = !isStarted;
       goButton.disabled = false;
+      intervalsLeftSpan.textContent = `Intervals left: ${intervalsAmount}`;
       clearInterval(updateTimeEachSecond);
     }
     let minutes = Math.floor(time / 60);
@@ -65,7 +71,7 @@ const updateTime = input => {
     timeLeftSpan.textContent = `${minutes >= 10 ? minutes : "0" + minutes}:${
       seconds >= 10 ? seconds : "0" + seconds
     }`;
-  }, 100);
+  }, 10);
 };
 
 const startApp = e => {
