4d3657c2346a5a083c19f0afa6b1011dd55f6279
kazbag <45149138+kazbag@users.noreply.github.com>
2019-11-23 13:19:06 +0100

    added modal

---
+++ roller/app.js
@@ -0,0 +1,21 @@
+const inputTopic = document.querySelector("#input-topic");
+const inputNick = document.querySelector("#input-nick");
+const list = document.querySelector(".list");
+const buttonAdd = document.querySelector("#button-topic");
+const modal = document.querySelector(".modal");
+const showModal = () => {
+  modal.classList.add("modal--visible");
+  setTimeout(() => {
+    modal.classList.remove("modal--visible");
+  }, 3000);
+};
+
+const addItem = e => {
+  e.preventDefault();
+  if (inputTopic.value.length >= 5) {
+    return (list.innerHTML += `<li class="item">${inputTopic.value} <button class="vote">Chcę to</button></li>`);
+  }
+  showModal();
+};
+
+buttonAdd.addEventListener("click", addItem);
