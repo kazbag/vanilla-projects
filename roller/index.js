6f59942b1de47b768ba36b0a2643887070f4809a
kazbag <45149138+kazbag@users.noreply.github.com>
2019-11-25 16:04:27 +0100

    added post method

--- roller/index.js
+++ roller/index.js
@@ -43,6 +43,17 @@ app.put("/:id", (req, res) => {
     );
 });
 
+app.post('/', (req, res) => {
+  const userInput = req.body;
+  db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+    if (err)
+      console.log(err);
+    else {
+      res.json({ result: result, document: result.ops[0] })
+    }
+  })
+})
+
 db.connect(err => {
   if (err) {
     console.log("nie można połączyć z bazą");
