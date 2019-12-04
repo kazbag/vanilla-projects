a0e5689675d56dae3b6564dc00885ee0e53903f7
kazbag <45149138+kazbag@users.noreply.github.com>
2019-12-04 12:18:00 +0100

    created some styles and sticky nav

---
+++ volvo-fansite/app.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.nav')
+let sticky = navbar.offsetTop
+window.onscroll = () => {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add('sticky')
+    }
+    else
+        navbar.classList.remove('sticky')
+}
