54ea97506a355650696618604ba793d61876b942
kazbag <45149138+kazbag@users.noreply.github.com>
2019-12-12 20:03:16 +0100

    changed html values, refactor of js

--- weather/app.js
+++ weather/app.js
@@ -2,13 +2,10 @@ const api_key = "d9b6b27164757a07549519653faf6737";
 const searchbox = document.querySelector("#searchbox");
 const searchButton = document.querySelector("#search");
 const cardContainer = document.querySelector(".card-container");
-let city = "";
-const getCityValue = e => {
-  city = e.target.value;
-};
 
 const getWeather = async e => {
   e.preventDefault();
+  const city = searchbox.value
   const api_call = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric&lang=pl`
   );
@@ -17,30 +14,29 @@ const getWeather = async e => {
 };
 
 const getCard = response => {
+  cardContainer.innerHTML = ''
   if (response.cod === 200) {
-    cardContainer.innerHTML = `
-    <div class="card">
+    const card = document.createElement('div')
+    card.innerHTML =
+      `<div class="card">
         <div class="city">
-        <img class="city-image" src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png"/>
-            <h3 class="city-name">${response.name}</h3>
+          <img class="city-image" src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" />
+          <h3 class="city-name">${response.name}</h3>
         </div>
         <div class="city-info">
-            <h6>${response.weather[0].description}</h6>
-            <h6 class="city-temperature">
-                ${response.main.temp}&deg;C
-            </h6>
-            <h6 class="city-temperature">
-                Wilgotność: ${response.main.humidity}%
-            </h6>
+          <h6>${response.weather[0].description}</h6>
+          <h6 class="city-temperature">
+            ${response.main.temp}&deg;C
+              </h6>
+          <h6 class="city-temperature">
+            Wilgotność: ${response.main.humidity}%
+              </h6>
         </div>
-
-    </div>
-    `
-
+      </div>`
+    cardContainer.appendChild(card)
   } else {
     cardContainer.innerHTML = "Podaj prawidłowe miasto"
   }
 };
 
-searchbox.addEventListener("input", getCityValue);
 searchButton.addEventListener("click", getWeather);
