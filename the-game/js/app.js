4c20a7e5af4ce85db11bdbe8b7a131825e9256a2
kazbag <45149138+kazbag@users.noreply.github.com>
2019-12-15 18:49:55 +0100

    added monster class

--- the-game/js/app.js
+++ the-game/js/app.js
@@ -1,8 +1,9 @@
 class Player {
-    constructor(nick, password, email, level, gold, defense, attack, minDamage, maxDamage, strength, dexterity, isPremium) {
+    constructor(nick, password, email, id, level, gold, defense, attack, minDamage, maxDamage, strength, dexterity, isPremium) {
         this.nick = nick;
         this.password = password;
         this.email = email;
+        this.id = id;
         this.level = level;
         this.gold = gold;
         this.defense = defense;
@@ -19,6 +20,7 @@ const player = {
     nick: 'Player 1',
     password: 'veryhardpassword',
     email: 'email@gmail.com',
+    id: 12,
     level: 10,
     gold: 1000,
     defense: 260,
@@ -30,3 +32,22 @@ const player = {
     isPremium: false
 }
 
+class Monster {
+    constructor(name, level, defense, attack, minDamage, maxDamage, strength, dexterity, drop) {
+        this.name = name;
+        this.level = level;
+        this.defense = defense;
+        this.attack = attack;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.drop = drop;
+    }
+    roar() {
+        console.log(`${this.name} says Ssshhh...`);
+    }
+}
+
+const rat = new Monster("Rat", 1, 10, 10, 0, 4, 5, 5, ['cheese', 'gold coin']);
+rat.roar()
