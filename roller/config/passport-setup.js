a721bfc09b7862ac15131773ea335827fd2edec8
kazbag <45149138+kazbag@users.noreply.github.com>
2020-01-07 15:00:03 +0100

    added simple callback

--- roller/config/passport-setup.js
+++ roller/config/passport-setup.js
@@ -1,13 +1,17 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./keys");
 
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret,
-        callbackURL: '/auth/google/redirect'
-    }, () => {
-//callback
-    })
+  new GoogleStrategy(
+    {
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret,
+      callbackURL: "/auth/google/redirect"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      //callback
+      console.log(profile);
+    }
+  )
 );
--- roller/routes/auth-routes.js
+++ roller/routes/auth-routes.js
@@ -1,17 +1,23 @@
-const router = require('express').Router()
-const passport = require('passport')
+const router = require("express").Router();
+const passport = require("passport");
 
 // login
-router.get('/login', (req,res)=>{
-    res.render('login')
-})
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 // google
-router.get('/google',passport.authenticate('google',{
-    scope: ['profile']
-}))
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  })
+);
 // logout
-router.get('/logout', (req,res)=>{
-    res.send('wylogowywanie')
-})
-
-module.exports = router
+router.get("/logout", (req, res) => {
+  res.send("wylogowywanie");
+});
+// callback
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  res.send("dostałeś callbacka");
+});
+module.exports = router;
